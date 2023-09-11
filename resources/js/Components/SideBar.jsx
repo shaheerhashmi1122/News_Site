c17a72c73d7798d9d73db4e9b39fe0e708405036
Abdul Khaliq <108611214+Abdulkhaliq9@users.noreply.github.com>
2023-09-11 03:58:34 -0700

    profile setting in side bar

--- resources/js/Components/SideBar.jsx
+++ resources/js/Components/SideBar.jsx
@@ -86,17 +86,10 @@ export default function SideBar({ darkMode, user }) {
                         <li>
                             <Dropdown>
                                 <Dropdown.Trigger>
-                                    <span className="inline-flex rounded-md">
-                                        <button
-                                            type="button"
-                                            className="inline-flex items-center px-1 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                                        >
+                                  
                                     <img className="setting" src={Setting} alt="" />
-                                            {user}
-                                        </button>
-                                    </span>
                                 </Dropdown.Trigger>
-                                <Dropdown.Content>
+                                <Dropdown.Content  >
                                     <Dropdown.Link href={route("profile.edit")}>
                                         Profile
                                     </Dropdown.Link>
