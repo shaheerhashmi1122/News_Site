7bd1d266e5c21fae704a67eb5a6902ffc22fac99
Abdul Khaliq <108611214+Abdulkhaliq9@users.noreply.github.com>
2023-09-11 03:38:44 -0700

    side bar postion set

--- resources/js/Components/SideBar.jsx
+++ resources/js/Components/SideBar.jsx
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import Dropdown from "@/Components/Dropdown";
 import Home from "@/assests/home.png";
 import Table from "@/assests/table.png";
 import Charts from "@/assests/charts.png";
 import Pages from "@/assests/pages.png";
 import Dash from "@/assests/dash.png";
+import Setting from "@/assests/setting.png";
 import { Link } from "@inertiajs/react";
 
 import "../../css/app.css";
+import DarkMode from "./DarkMode";
 
-export default function SideBar() {
+export default function SideBar({ darkMode, user }) {
+    // ===============================darkmode============================
+
     return (
-    
-            <aside className=" min-h-screen border-r w-10 border-gray-100">
-            
-            
-          
-                <nav className="mt-6">
-                   <div>
-                   <ul className="side-menu ">
+        <aside
+            className={`${
+                darkMode ? "dark-theme" : "light-theme"
+            } bg-white min-h-screen side-bar border-r border-gray-100`}
+        >
+            <nav>
+                <div>
+                    <ul className="side-menu ">
                         <li>
                             <Link href={route("dashboard")}>
                                 <img
@@ -73,11 +78,41 @@ export default function SideBar() {
                                 Pages
                             </Link>
                         </li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li>
+                            <Dropdown>
+                                <Dropdown.Trigger>
+                                    <span className="inline-flex rounded-md">
+                                        <button
+                                            type="button"
+                                            className="inline-flex items-center px-1 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                        >
+                                    <img className="setting" src={Setting} alt="" />
+                                            {user.name}
+                                        </button>
+                                    </span>
+                                </Dropdown.Trigger>
+                                <Dropdown.Content>
+                                    <Dropdown.Link href={route("profile.edit")}>
+                                        Profile
+                                    </Dropdown.Link>
+                                    <Dropdown.Link
+                                        href={route("logout")}
+                                        method="post"
+                                        as="button"
+                                    >
+                                        Log Out
+                                    </Dropdown.Link>
+                                </Dropdown.Content>
+                            </Dropdown>
+                        </li>
                     </ul>
-                   </div>
-                </nav>
-            </aside>
-        
+                </div>
+            </nav>
+        </aside>
     );
 }
-
