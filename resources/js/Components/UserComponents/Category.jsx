02eeafdd7ae905e6e1768108cb929741094d31c3
Abdul Khaliq <108611214+Abdulkhaliq9@users.noreply.github.com>
2023-10-21 22:58:42 +0500

    pagination commit

---
+++ resources/js/Components/UserComponents/Category.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import "../../../css/User/Category.css"
+
+export default function Category() {
+  return (
+    <>
+       <div className="container">
+<div className="row">
+<div className="col-sm-4">
+
+	<div className="single category">
+		<h3 className="side-title">Category</h3>
+		<ul className="list-unstyled">
+			<li><a href="" title="">Business <span className="pull-right">13</span></a></li>
+			<li><a href="" title="">Technology <span className="pull-right">13</span></a></li>
+			<li><a href="" title="">Web <span className="pull-right">13</span></a></li>
+			<li><a href="" title="">Ecommerce <span className="pull-right">13</span></a></li>
+			<li><a href="" title="">Wordpress <span className="pull-right">13</span></a></li>
+			<li><a href="" title="">Android <span className="pull-right">13</span></a></li>
+			<li><a href="" title="">IOS <span className="pull-right">13</span></a></li>
+			<li><a href="" title="">Windows <span className="pull-right">13</span></a></li>
+		</ul>
+   </div>
+</div> 
+</div>
+</div> 
+    </>
+  )
+}
